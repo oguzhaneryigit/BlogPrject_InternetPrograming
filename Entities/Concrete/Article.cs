1673914dbbdea05dba08c8331948ba7030871247
oguzhan eryigit <oguzhanerygt@gmail.com>
2021-12-15 18:42:27 +0300

    Add project files.

---
+++ Entities/Concrete/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    class Article
+    {
+        public int ArticleId { get; set; }
+        public int AuthorId { get; set; }
+        public DateTime ReleaseTime { get; set; }
+        public string Heading { get; set; }
+        public string Type { get; set; }
+        public string Content { get; set; }
+
+    }
+}
